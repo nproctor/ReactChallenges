15978068f4aba47eb89ed293f1b550950e7059a7
Natasha Proctor <natasharproctor@gmail.com>
2024-08-13 20:12:28 -0700

    Add ToDo List component to Appp

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import ShowHideTitle from './ShowHideTitle';
 import Timer from './Timer';
+import ToDoList from './ToDoList';
 
 function App() {
   return (
     <div className="App">
       <ShowHideTitle/>
       <Timer/>
+      <ToDoList/>
     </div>
   );
 }
